55fea26bcedad93e371a2530f88174486d428a99
abdullah-al-mohammad <abdullahalmuhammad6@gmai.com>
2026-03-23 22:26:35 +0600

    Implement search functionality in AllApps component to filter applications by title

--- src/pages/AllApps/AllApps.jsx
+++ src/pages/AllApps/AllApps.jsx
@@ -1,16 +1,47 @@
+import { useState } from 'react';
+import { IoIosSearch } from 'react-icons/io';
 import useData from '../../hooks/useData/useData';
 import ShowAllData from './ShowAllData';
 
 const AllApps = () => {
   const allApps = useData();
+  // const inputRef = useRef('');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const filterApps = allApps.filter(app =>
+    app.title.toLowerCase().includes(searchQuery.toLocaleLowerCase())
+  );
+
+  if (filterApps.length === 0) {
+    return <div className="col-span-full text-center text-gray-500 mt-4">No App Found</div>;
+  }
+
   return (
     <div>
       <h1 className="mb-4 text-4xl text-center font-semibold">Our All Applications</h1>
       <p className="mb-12 text-center text-Slate-Blue">
         Explore All Apps on the Market developed by us. We code for Millions
       </p>
+      <div className="flex justify-between items-center">
+        <h4 className="font-bold mb-8">({allApps.length}) Apps Found</h4>
+        <div>
+          <label className="input">
+            <IoIosSearch />
+            <input
+              // ref={inputRef}
+              value={searchQuery}
+              onChange={e => setSearchQuery(e.target.value)}
+              type="search"
+              name="search"
+              placeholder="Search Apps"
+              id=""
+              className="px-5 rounded grow"
+            />
+          </label>
+        </div>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-        {allApps.map(app => (
+        {filterApps.map(app => (
           <ShowAllData key={app.id} app={app}></ShowAllData>
         ))}
       </div>
