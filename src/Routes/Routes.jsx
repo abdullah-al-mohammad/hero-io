34fec240e650ad826cb2df795a79661e42844b39
abdullah-al-mohammad <abdullahalmuhammad6@gmai.com>
2026-03-23 22:26:47 +0600

    Add ErrorPage component to router for improved error handling

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from 'react-router';
 import Root from '../Root/Root';
 import AllApps from '../pages/AllApps/AllApps';
 import AppDetails from '../pages/AppDetails/AppDetails';
+import ErrorPage from '../pages/ErrorPage/ErrorPage';
 import Home from '../pages/Home/Home';
 
 export const router = createBrowserRouter([
   {
     path: '/',
+    errorElement: <ErrorPage />,
     Component: Root,
     children: [
       {
