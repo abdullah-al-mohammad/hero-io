c32c5bac9aab2320bbc42e45f4ed296a1cad3419
abdullah-al-mohammad <abdullahalmuhammad6@gmai.com>
2026-03-23 09:49:33 +0600

    useData hook to manage state and fetch data from JSON

--- src/hooks/useData/useData.jsx
+++ src/hooks/useData/useData.jsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from 'react';
+
 const useData = () => {
-  const heroDataPromise = fetch('./data.json').then(res => res.json());
-  // console.log(heroDataPromise);
-  return heroDataPromise;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('./data.json')
+      .then(res => res.json())
+      .then(result => setData(result))
+      .catch(err => console.error(err));
+  }, []);
+
+  return data;
 };
 
 export default useData;
