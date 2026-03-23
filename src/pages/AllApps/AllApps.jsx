f36049504fd1b6f33f3063538903b1c0b875dcdf
abdullah-al-mohammad <abdullahalmuhammad6@gmai.com>
2026-03-23 10:01:46 +0600

    Enhance AllApps and ShowAllData components with improved styling and functionality

--- src/pages/AllApps/AllApps.jsx
+++ src/pages/AllApps/AllApps.jsx
@@ -5,9 +5,11 @@ const AllApps = () => {
   const allApps = useData();
   return (
     <div>
-      <h1>Our All Applications</h1>
-      <p>Explore All Apps on the Market developed by us. We code for Millions</p>
-      <div>
+      <h1 className="mb-4 text-4xl text-center font-semibold">Our All Applications</h1>
+      <p className="mb-12 text-center text-Slate-Blue">
+        Explore All Apps on the Market developed by us. We code for Millions
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {allApps.map(app => (
           <ShowAllData key={app.id} app={app}></ShowAllData>
         ))}
--- src/pages/AllApps/ShowAllData.jsx
+++ src/pages/AllApps/ShowAllData.jsx
@@ -1,8 +1,43 @@
-const ShowAllData = ({ app }) => {
-  // const apps = use(app);
-  console.log(app);
+import { Link } from 'react-router';
+import downloadImg from '../../assets/icon-downloads.png';
+import rating from '../../assets/icon-ratings.png';
 
-  return <div>ShowAllData</div>;
+const ShowAllData = ({ app }) => {
+  const { image, title, downloads, ratingAvg, id } = app;
+
+  const formatLargeNumber = num => {
+    const formatter = new Intl.NumberFormat('en-US', {
+      notation: 'compact',
+      compactDisplay: 'short',
+    });
+
+    return formatter.format(num);
+  };
+  const download = downloads * 100;
+  return (
+    <>
+      <Link to={`/details/${id}`}>
+        <div className="card bg-base-100 shadow-sm">
+          <figure>
+            <img className="w-40" src={image} alt="apps" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{title}</h2>
+            <div className="card-actions justify-between">
+              <div className="badge bg-[#F1F5E8] text-[#00D390]">
+                <img className="w-4" src={downloadImg} alt="" />
+                {formatLargeNumber(download)}
+              </div>
+              <div className="badge bg-[#FFF0E1] text-[#FF8811]">
+                <img className="w-4" src={rating} alt="" />
+                {ratingAvg}
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </>
+  );
 };
 
 export default ShowAllData;
