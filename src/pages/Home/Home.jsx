92bf54fbad53314eb54eda3eb51cc3bd14396edf
abdullah-al-mohammad <abdullahalmuhammad6@gmai.com>
2026-03-26 12:13:48 +0600

    Home component to include loading state and update data handling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,19 @@ import useData from '../../hooks/useData/useData';
 import Banner from './Banner/Banner';
 import TopAppsSection from './TopAppsSection/TopAppsSection';
 
-// const heroDataPromise = fetch('./data.json').then(res => res.json());
 const Home = () => {
-  const data = useData();
+  const { data, loading } = useData();
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <span className="loading loading-spinner loading-xl"></span>
+      </div>
+    );
+  }
   return (
     <div className="space-y-10">
       <Banner />
-      <TopAppsSection heroDataPromise={data} />
+      <TopAppsSection data={data} />
     </div>
   );
 };
